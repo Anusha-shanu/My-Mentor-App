b78ebdb55e05ef33ce620021bad98202fa7580c3
Anusha Mohan T <anoha1531@gmail.com>
2025-08-14 15:05:23 +0530

    final2

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,4 +1,4 @@
 // const API_BASE_URL = "http://127.0.0.1:8000";
 //const API_BASE_URL = "http://localhost:5000";
-const API_BASE_URL = "https://my-mentor-rbkh.onrender.com";
+const API_BASE_URL = "https://my-mentor-rbkh.onrender.com/api";
 export default API_BASE_URL;
